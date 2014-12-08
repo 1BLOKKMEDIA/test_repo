3e008d973a285c5e89b91ae32ed0e1632d47ad4e
1BLOKKMEDIA <cseti@blokkmedia.com>
2014-12-08 11:17:53 +0100

    ez a jo

--- index.php
+++ index.php
@@ -3,10 +3,6 @@
 		<title></title>
 	</head>
 	<body>
-<<<<<<< HEAD
-		<h2>test_bra</h2>
-=======
-		<h2>test branch</h2>
->>>>>>> 1558dd40064cf3914aa86d865c2d3e486f5fe3b4
+		<h2>test_bra ez a jó</h2>
 	</body>
 </html>
